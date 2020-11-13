e07f0192ca355f948a0831d61f4fbf1bb79814d4
Luís Linhares <luislinhares@cefetmg.br>
2020-11-12 21:43:42 -0300

    Criação de todas as deposições

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from numpy import random
 import time
 import matplotlib.pyplot as plt
 import math
+import csv
+
+
+
+
 
 # Algoritmo que faz a relaxaçao superficial
 # E usado junto da deposicao aleatoria, para atingir o ponto de rugosidade
@@ -81,11 +86,9 @@ def rugosidade(deposicao):
     tamanho_deposicao = len(deposicao)
     # print(tamanho_amostra)
     erro = 0
-    contador = 0
     media = altura_media(deposicao)
     for i in range(1, tamanho_deposicao):
         erro = erro + np.power((deposicao[i] - media), 2)
-        contador = contador + 1
 
     # print(math.sqrt(erro/(tamanho_deposicao - 1)))
     return math.sqrt(erro / (tamanho_deposicao - 1))
@@ -114,63 +117,63 @@ def lcg(modulo, a, b, aleatorio):
 # l = tamanho da amostra
 # tempo = sempre em 10^tempo
 
-def do_deposicao(l=200, tempo=5, tipo_deposicao=0, grafico_instantaneo=False):
+def do_deposicao(l=200, tempo_fim = 0, tipo_deposicao=0, grafico_instantaneo=False, aleatorio=0):
 
     tempo_instantaneo = 25
     max_tempo_instantaneo = 150
-    amostras_independentes = 1
-
-    if not grafico_instantaneo:
-        # Mudar aqui a quantidade de amostras independentes
-        amostras_independentes = np.power(1, 1)
-
 
     m = np.power(2, 31)
     a = 843314861
     b = 453816693
+    #aleatorio = lcg(m, a, b, aleatorio)
+
+    amostras_independentes = 1
+
+    l_instantaneo = []
+
     deposicao = np.zeros(l + 1)
+    #l_deposicao_amostra_individual = np.zeros((tempo_fim, l + 1), dtype='uint8')
+    l_deposicao_amostra_individual = []
+    contador = 0
+    contador_amostra = 0
 
+    for tf in range(tempo_fim):
+        for i in range(l):
 
-    # para executar a média das amostras
-    l_rugosidade_total = []
+            x3 = (l - 1) / m * aleatorio + 1
+            x3 = round(x3)
+            x3 = int(x3)
 
-    tempo_fim = np.power(10, tempo)
-    semente = 0
-    aleatorio = lcg(m, a, b, semente)
+            if tipo_deposicao == 1:
+                deposicao = do_relaxacao_superficial(deposicao, x3)
+            elif tipo_deposicao == 2:
+                deposicao = do_deposicao_balistica(deposicao, x3)
+            else:
+                deposicao[x3] = deposicao[x3] + 1
 
-    for amostra in range(amostras_independentes):
+            aleatorio = lcg(m, a, b, aleatorio)
 
-        contador = 0
-        contador_amostra = 0
-        l_instantaneo = []
-        l_rugosidade = []
-        while (contador < tempo_fim):
-            for i in range(l):
-                x3 = (l - 1) / m * aleatorio + 1
-                x3 = round(x3)
-                x3 = int(x3)
-                if tipo_deposicao == 1:
-                    deposicao = do_relaxacao_superficial(deposicao, x3)
-                elif tipo_deposicao == 2:
-                    deposicao = do_deposicao_balistica(deposicao, x3)
-                else:
-                    deposicao[x3] = deposicao[x3] + 1
+        if grafico_instantaneo:
+            if contador_amostra == tempo_instantaneo and tf <= max_tempo_instantaneo:
+                l_instantaneo.append(deposicao.copy())
+                contador_amostra = 0
 
-                aleatorio = lcg(m, a, b, aleatorio)
+        l_deposicao_amostra_individual.append(deposicao.copy())
 
-            if grafico_instantaneo:
-                if contador_amostra == tempo_instantaneo and contador <= max_tempo_instantaneo:
-                    l_instantaneo.append(deposicao.copy())
-                    contador_amostra = 0
+        #contador = contador + 1
+        contador_amostra = contador_amostra + 1
 
-            l_rugosidade.append(deposicao.copy())
+    """
+        #média entre as amostras
+        for temp in range(len(l_deposicao_amostra_individual)):
+            tamanho_deposicao = len(l_deposicao_amostra_individual[temp])
+            for dep in range(1, tamanho_deposicao):
+                arr_amostras_media[temp][dep - 1] = arr_amostras_media[temp][dep - 1] + (l_deposicao_amostra_individual[temp][dep]/amostras_independentes)
 
-            contador = contador + 1
-            contador_amostra = contador_amostra + 1
-
-        l_rugosidade_total.append(l_rugosidade.copy())
-
-    return l_instantaneo, l_rugosidade_total
+    for med in range(len(arr_amostras_media)):
+        l_rugosidade.append(rugosidade(arr_amostras_media[med]))    
+    """
+    return l_instantaneo, l_deposicao_amostra_individual, aleatorio
 
 
 def gera_grafico_instantaneo(l_instantaneo):
@@ -192,9 +195,8 @@ def gera_grafico_instantaneo(l_instantaneo):
     plt.legend(l_legenda, bbox_to_anchor=(1, 1.02), loc="upper left")
     plt.show()
 
-def gera_grafico_rugosidade(l_rugosidade):
 
-    plt.figure(figsize=(14, 5))
+def rugosidade_media_amostras(l_rugosidade):
 
     tempo = len(l_rugosidade[0])
     tamanho_substrato = len(l_rugosidade[0][0])
@@ -213,33 +215,138 @@ def gera_grafico_rugosidade(l_rugosidade):
     for i in range(len(arr_rugosidade_media)):
         l_rugosidade_media.append(rugosidade(arr_rugosidade_media[i]))
 
+    return l_rugosidade_media
+
+
+def cria_csv(arr_amostras_media, tipo_deposicao):
+
+    l_rugosidade = []
+    for i in range(len(arr_amostras_media)):
+        l_rugosidade.append(rugosidade(arr_amostras_media[i]))
+
+    titulo_csv = 'rugosidade_'+tipo_deposicao
+
+    with open(titulo_csv+'.csv', 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for j in range(len(l_rugosidade)):
+            spamwriter.writerow([j + 1, l_rugosidade[j]])
+
+
+def gera_grafico_rugosidade(pasta_tipo_rugosidade, str_deposicao):
+
+    plt.figure(figsize=(14, 5))
+
+    l_deposicao = []
+    l_deposicao.append(200)
+    #l_deposicao.append(400)
+    #l_deposicao.append(800)
+    #l_deposicao.append(1600)
+
+    l_legenda = []
+    l_legenda.append('L = 200')
+    l_legenda.append('L = 400')
+    l_legenda.append('L = 800')
+    l_legenda.append('L = 1600')
+
+
+    for i in range(len(l_deposicao)):
+
+        deposicao = l_deposicao[i]
+        l_deposicao_grafico = []
+
+        with open('./'+str_deposicao+'_'+str(deposicao)+'.csv') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                l_deposicao_grafico.append(float(row[1]))
+
+        plt.plot(l_deposicao_grafico.copy())
+
     plt.xscale("log")
     plt.yscale("log")
-    plt.plot(l_rugosidade_media)
-    plt.legend(['L - '+str(tamanho_substrato - 1)])
+    plt.legend(l_legenda, bbox_to_anchor=(1, 1.02), loc="upper left")
+
     plt.show()
 
-    return False
+def media_amostras(l_deposito, arr_amostras_media, amostras_independentes):
+    for tempo in range((len(l_deposito))):
+        for dep in range(1, len(l_deposito[tempo])):
+            arr_amostras_media[tempo][dep - 1] = arr_amostras_media[tempo][dep - 1] + (
+                    l_deposito[tempo][dep] / amostras_independentes)
 
 
-    #plt.show()
+    return arr_amostras_media
 
 
-# Aleatória
-grafico_da_instantaneo, grafico_da_rugosidade = do_deposicao(500, 3, 0, True)
-gera_grafico_instantaneo(grafico_da_instantaneo)
 
-# Relaxação Superficial
-grafico_dars_instantaneo, grafico_dars_rugosidade  = do_deposicao(500, 3, 1, True)
-gera_grafico_instantaneo(grafico_dars_instantaneo)
 
-# Deposição Balística
-grafico_db_instantaneo, grafico_db_rugosidade  = do_deposicao(500, 3, 2, True)
-gera_grafico_instantaneo(grafico_db_instantaneo)
+#amostas independentes. Para 1000 amostras, basta elevar 10^3
+amostras_independentes = int(np.power(1, 3))
 
-#Rugosidade com L = 200 e tempo = 10^2, DARS
-grafico_dars_instantaneo, grafico_dars_rugosidade  = do_deposicao(200, 2, 1, False)
-gera_grafico_rugosidade(grafico_dars_rugosidade)
+#Tempo de cada substrato
+tempo_1600 = np.power(10, 6)
+tempo_800 = np.power(10, 5)
+tempo_400 = np.power(10, 5)
+tempo_200 = 11500
+
+
+l_1600 = 1600
+l_800 = 800
+l_400 = 400
+l_200 = 200
+
+arr_amostras_media_1600 = np.zeros((tempo_1600, l_1600), dtype='f2')
+arr_amostras_media_800 = np.zeros((tempo_800, l_800), dtype='f2')
+arr_amostras_media_400 = np.zeros((tempo_400, l_400), dtype='f2')
+arr_amostras_media_200 = np.zeros((tempo_200, l_200), dtype='f2')
+
+m = np.power(2, 31)
+a = 843314861
+b = 453816693
+aleatorio = lcg(m, a, b, 0)
+
+#NA FUNÇÃO DA_DEPOSICAO, O 3º PARÃMETRO É O TIPO DE DEPOSICAO. 0 = ALEATÓRIA, 1 = DARS, 2 = DB
+for i in range(amostras_independentes):
+
+    """
+    print(str(i)+' amostra 1600')
+
+    l_da_instantaneo, l_da_deposito, aleatorio = do_deposicao(l_1600, tempo_1600, 1, False, aleatorio)
+    arr_amostras_media_1600 = media_amostras(l_da_deposito, arr_amostras_media_1600, amostras_independentes)
+
+    print(str(i)+' amostra 800')
+
+    l_da_instantaneo = None
+    l_da_deposito = None
+    l_da_instantaneo, l_da_deposito, aleatorio = do_deposicao(l_800, tempo_800, 1, False, aleatorio)
+    arr_amostras_media_800 = media_amostras(l_da_deposito, arr_amostras_media_800, amostras_independentes)
+
+    print(str(i)+' amostra 400')
+
+    l_da_instantaneo = None
+    l_da_deposito = None
+    l_da_instantaneo, l_da_deposito, aleatorio = do_deposicao(l_400, tempo_400, 1, False, aleatorio)
+    arr_amostras_media_400 = media_amostras(l_da_deposito, arr_amostras_media_400, amostras_independentes)
+    """
+
+    print(str(i)+' amostra 200')
+
+    l_da_instantaneo = None
+    l_da_deposito = None
+    l_da_instantaneo, l_da_deposito, aleatorio = do_deposicao(l_200, tempo_200, 1, False, aleatorio)
+    arr_amostras_media_200 = media_amostras(l_da_deposito, arr_amostras_media_200, amostras_independentes)
+
+
+#GERA TODOS OS CSVS
+
+#cria_csv(arr_amostras_media_1600, 'DEPOSICAO_ALEATORIA_RELAXACAO_SUPERFICIAL_1600')
+#cria_csv(arr_amostras_media_800, 'DEPOSICAO_ALEATORIA_RELAXACAO_SUPERFICIAL_800')
+#cria_csv(arr_amostras_media_400, 'DEPOSICAO_ALEATORIA_RELAXACAO_SUPERFICIAL_400')
+cria_csv(arr_amostras_media_200, 'DEPOSICAO_ALEATORIA_RELAXACAO_SUPERFICIAL_200')
+
+
+
+gera_grafico_rugosidade('', 'rugosidade_DEPOSICAO_ALEATORIA_RELAXACAO_SUPERFICIAL')
 
 
 # plt.xscale("log")
